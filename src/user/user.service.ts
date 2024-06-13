361189ef9ff59ddde9b453ee5e91f26e2127cf30
Daniil Sivtsov <danilklin17@gmail.com>
2024-06-13 18:49:31 +0300

    Add: readme and try catch block on user.service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
@@ -6,10 +6,14 @@ export class UserService {
   constructor(private readonly databaseService: DatabaseService) {}
 
   async resetProblemsFlag(): Promise<number> {
-    const res = await this.databaseService.user.updateMany({
-      where: { trouble: true },
-      data: { trouble: false },
-    });
-    return res.count;
+    try {
+      const res = await this.databaseService.user.updateMany({
+        where: { trouble: true },
+        data: { trouble: false },
+      });
+      return res.count;
+    } catch (error) {
+      throw new InternalServerErrorException('Error resetting troubles flag');
+    }
   }
 }
